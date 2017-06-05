f5d8013269ed0c598a03e3fae26abed5972969f3
Jason Miller <hack-r@users.noreply.github.com>
2017-06-04 20:02:28 -0400

    Create antisec_twitter.py

---
+++ antisec_twitter.py
@@ -0,0 +1,35 @@
+# Jason D. Miller
+# github.com/hack-r
+
+from twython import TwythonStreamer
+import RPi.GPIO as GPIO
+import time
+
+C_KEY =  ""
+C_SECRET = ""
+A_TOKEN = "-"
+A_SECRET = ""
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18,GPIO.OUT)
+
+def blink():
+    GPIO.output(18, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(18, GPIO.LOW)
+
+class MyStreamer(TwythonStreamer):
+     def on_success(self, data):
+         if 'text' in data:
+              blink()
+              print("Antisec Tweet detected. Call the FBI.")
+
+stream = MyStreamer(C_KEY, C_SECRET, A_TOKEN, A_SECRET)
+
+stream.statuses.filter(track="#antisec")
+
+
+
+
+
